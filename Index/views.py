eff02012300c96f29ceb2f4172633da69a64c0e5
DhuMuni <muniyappan.m1992@gmail.com>
2021-06-05 20:41:16 +0530

    RestAPI branch created

--- Index/views.py
+++ Index/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User,auth
 from django.contrib import messages
